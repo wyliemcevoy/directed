43488bce2c32c24429b2835cd235d4c259dd174f
Wylie McEvoy <wylie.seven@gmail.com>
2016-06-05 22:32:02 -0500

    Add lowest common ancestor function to BST.

--- directed/binary_tree.h
+++ directed/binary_tree.h
@@ -30,6 +30,11 @@ namespace directed {
 		std::vector<T> Inorder();
 		int GetHeight();
 
+		// Slow 
+		// Uses vectors 
+		// Could use arrays, but would require a O(h) pass of the tree to determine height
+		T GetLowestCommonAncestor(const T & one, const T & other);
+
 	private:
 		std::unique_ptr<TreeNode> root_;
 		int RecursiveHeight(TreeNode * node);
@@ -37,6 +42,9 @@ namespace directed {
 		static void RecursivePreorder(std::vector<T> * build, TreeNode * node);
 		static void RecursivePostorder(std::vector<T> * build, TreeNode * node);
 		static void RecursiveInorder(std::vector<T> * build, TreeNode * node);
+		static void RecursiveGetPath(std::vector<T> * path, TreeNode * current, const T & target);
+		std::vector<T> GetPath(const T & target);
+
 
 	};
 
@@ -87,6 +95,83 @@ namespace directed {
 		}
 		return true;
 	}
+
+	template<class T>
+	T BinaryTree<T>::GetLowestCommonAncestor(const T & one, const T & other)
+	{
+		// Handle empty tree scenario
+		if (root_ == nullptr)
+		{
+			return one;
+		}
+
+		std::vector<T> one_path = GetPath(one);
+		std::vector<T> other_path = GetPath(other);
+
+		// Handle bad input (one or other was not in the tree)
+		if (one_path.empty())
+		{
+			return one;
+		}
+		if (other_path.empty())
+		{
+			return other;
+		}
+
+		T * current_ancestor = & root_.get()->data;
+		int i = 0;
+
+		while ((one_path.size() > i ) && (other_path.size() > i ) && one_path[i] == other_path[i] )
+		{
+			current_ancestor = & one_path[i];
+			i++;
+		}
+
+		return * current_ancestor;
+	}
+
+
+	template<class T>
+	std::vector<T> directed::BinaryTree<T>::GetPath(const T & target)
+	{
+		std::vector<T> path;
+		RecursiveGetPath(&path, root_.get(), target);
+
+		return path;
+	}
+
+	template<class T>
+	void directed::BinaryTree<T>::RecursiveGetPath(std::vector<T> * path, TreeNode * current, const T & target)
+	{
+		if (current != nullptr)
+		{
+			path->push_back(current->data);
+
+			if (current->data == target)
+			{
+				// Path found
+				return;
+			}
+			else {
+				if (current->data > target)
+				{
+					// recurse left
+					RecursiveGetPath(path, current->left.get(), target);
+				}
+				else {
+					// recurse right
+					RecursiveGetPath(path, current->right.get(), target);
+				}
+			}
+		}
+		else
+		{
+			// The target does not exist in this tree, return an empty path for failure.
+			path->clear();
+		}
+	}
+
+
 
 	template<class T>
 	std::vector<T> BinaryTree<T>::Preorder()
