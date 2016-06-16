409c225414f0f75b5a315e15aefdb364bea6313b
Wylie McEvoy <wylie.seven@gmail.com>
2016-06-15 23:58:41 -0500

    Add Kadane Algorithm for max contiguous subarray

---
+++ directed/kadane.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+
+class Kadane
+{
+public:
+	Kadane();
+	~Kadane();
+};
+
+
+namespace puzzles {
+	int ExplainedKadane(const std::vector<int> & data);
+}
+
+
